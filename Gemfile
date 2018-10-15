a75cbbc51f15033402cf833499ed8da44644d740
Juan Mejia <juan00xt@gmail.com>
2018-10-15 17:41:36 -0500

    Add webpacker-react gem

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'webpacker', '~> 3.5'
+gem 'webpacker-react', "~> 0.3.1"
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import PropTypes from 'prop-types'
 import { HelloReact } from '../src/components/hello_react'
+import WebpackerReact from 'webpacker-react'
 
-document.addEventListener('DOMContentLoaded', () => {
-  render(
-    <HelloReact name="React" />,
-    document.getElementById('root')
-  )
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   render(
+//     <HelloReact name="React" />,
+//     document.getElementById('root')
+//   )
+// })
+
+WebpackerReact.setup({HelloReact})
